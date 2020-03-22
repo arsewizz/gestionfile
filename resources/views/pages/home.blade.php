d966f046ad0060e807cbf4f2a8f6628aafda00ea
arsene <mwapearsene2014@gmail.com>
2020-03-22 08:55:01 +0100

    boostrap work good

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,10 @@
-<div class="container">
-    <div class="row">
-        <h3>BIENVENUE SUR L'APPLICATION</h3>
+@extends('layouts.app')
+@section('title','welcome sur notre page')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h3>BIENVENUE SUR L'APPLICATION</h3>
+        </div>
     </div>
-</div>
+@endsection

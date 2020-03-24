8b61f017c8ec02860cc1f98bfc49e9e2acb9910e
arsene <mwapearsene2014@gmail.com>
2020-03-24 14:09:08 +0100

    add collective html

--- resources/views/archives/create.blade.php
+++ resources/views/archives/create.blade.php
@@ -1,10 +1,23 @@
-<h2>Ajouter votre archives</h2>
+@extends('layouts.app')
+@include('layouts._authbar')
 
-<form action="/archives" method="post">
-    @csrf
-    <input type="text" name="intitule_archive" id="intitule_archive"> <br>
-    <input type="text" name="traitement" id="traitement"><br>
-    <input type="text" name="fichier" id="fichier"><br>
-    <input type="text" name="type" id="type">
-    <button type="submit">Enregidtrer</button>
-</form>
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <h3>Ajouter votre achive</h3>
+            </div>
+            {{--
+                <h2>Ajouter votre archives</h2>
+                    <form action="/archives" method="post">
+                        @csrf
+                        <input type="text" name="intitule_archive" id="intitule_archive"> <br>
+                        <input type="text" name="traitement" id="traitement"><br>
+                        <input type="text" name="fichier" id="fichier"><br>
+                        <input type="text" name="type" id="type">
+                        <button type="submit">Enregidtrer</button>
+                    </form>
+                --}}
+        </div>
+    </div>
+@endsection
--- resources/views/archives/index.blade.php
+++ resources/views/archives/index.blade.php
@@ -1,3 +1,12 @@
-List des Archives
+@extends('layouts.app')
+@include('layouts._authbar')
 
-<a href="/archives/create">Ajouter une archives</a>
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <a class="btn btn-flat" href="/archives/create">Ajouter uner archive</a>
+            </div>
+        </div>
+    </div>
+@endsection
